af6b426021f510877704f33f9cff8dce9a47d29b
srikanth <srikanth@192.168.0.9>
2017-06-24 08:50:04 +0530

    master check-in

---
+++ src/main/java/com/valid/Validation.java
@@ -0,0 +1,21 @@
+package com.valid;
+
+public class Validation {
+
+	public boolean validateName(String name){
+		if(name == null) {
+			return false;
+		}
+		if(name.isEmpty()){
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean mobileNumberValidation(String mobile){
+		if(mobile.length() != 10) {
+			return false;
+		}
+		return true;
+	}
+}
